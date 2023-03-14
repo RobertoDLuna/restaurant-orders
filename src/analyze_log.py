3e766223a30c62e8a13149359a404932273f5058
Roberto Luna <robertocgw@gmail.com>
2023-03-14 17:22:37 -0300

    feat: adição da func get_unique_foods

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -15,5 +15,21 @@ def get_data(path):
 # teste
 # print(get_data('data/orders_1.csv'))
 
+# Retornando refeições unicas
+
+def get_unique_foods(data):
+    # utilizando o i[1] para acessar a comida
+    # array padrão de saida da get_data: ['nome', 'comida', 'dia']
+    foods_data = [element[1] for element in data]
+    # utilando o set(array) pois não retornarão valores repetidos
+    # transformando em um array de unique itens
+    foods_unique = set(foods_data)
+    return foods_unique
+
+
+# teste
+print(get_unique_foods(get_data('data/orders_1.csv')))
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
