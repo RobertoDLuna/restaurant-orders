ad8ef898aec562f1cf514243ed682bce97de9c45
Roberto Luna <robertocgw@gmail.com>
2023-03-14 19:25:30 -0300

    feat: adição das funções food_never_requested_by_joao e days_that_joao_never_went

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -87,5 +87,36 @@ def count_hamburguer_requested_by_arnaldo(data):
     return arnaldo_count_hamburguer
 
 
+# Retornando comida que joao nunca pediu
+
+
+def food_never_requested_by_joao(data):
+    unique_foods = get_unique_foods(data)
+    joao_count_food = get_food_count_by_person(data, 'joao')
+    food_never_requested_by_joao = set([
+        food for food in unique_foods if food not in joao_count_food.keys()
+    ])
+    return food_never_requested_by_joao
+
+# Retornando doas que joão nunca foi na lanchonete
+
+
+def days_that_joao_never_went(data):
+    unique_days = get_unique_days(data)
+
+    # filtra os arrays que contem joao
+    foods_joao = [food for food in data if food[0] == 'joao']
+
+    # retorna os dias de joao
+    food_days_joao = [foods_joao[2] for food_joao in foods_joao]
+
+    # Retorna quais dias do joao não constam no dias unicos
+    joao_not_food_days = set([
+        day for day in unique_days if day not in food_days_joao
+    ])
+
+    return joao_not_food_days
+
+
 def analyze_log(path_to_file):
     raise NotImplementedError
