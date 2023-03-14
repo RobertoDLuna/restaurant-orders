e5f674bfb0b12e962d9debbedd0700329c223044
Roberto Luna <robertocgw@gmail.com>
2023-03-14 17:03:14 -0300

    feat: func get_data

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,19 @@
-# começando
+import csv
+
+# Importando os dados do arquivo
+
+
+def get_data(path):
+    with open(path) as file:  # abrindo o arquivo
+        file_readed = csv.reader(file)  # leitura de arquivo
+        # converte para um array, contendo arrays
+        #  com as informaçoes de cada linha
+        data = [row for row in file_readed]
+    return data
+
+
+# teste
+# print(get_data('data/orders_1.csv'))
 
 def analyze_log(path_to_file):
     raise NotImplementedError
